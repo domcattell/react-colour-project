319dc25de5f3a8635b47864314350f8b8041220e
domcattell <55451364+domcattell@users.noreply.github.com>
2020-01-14 15:40:45 +0000

    added colorhelpers

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
 import Pallete from './components/Pallete'
 import seedColors from './seedColors'
+import {generatePallete} from './colorHelpers';
 
 export class App extends Component {
     render() {
+        console.log(generatePallete(seedColors[3]))
         return (
             <div>
                 <Pallete {...seedColors[3]}/>
--- src/components/ColorBox.js
+++ src/components/ColorBox.js
@@ -3,11 +3,29 @@ import {CopyToClipboard} from 'react-copy-to-clipboard'
 import '../styles/ColorBox.css'
 
 export class ColorBox extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {copied: false}
+        this.changeCopyState = this.changeCopyState.bind(this);
+    }
+
+    changeCopyState() {
+        this.setState({copied: true}, () => {
+            setTimeout(() => this.setState({copied: false}), 1500)
+        });
+    }
+
     render() {
         const {name, background} = this.props;
+
         return (
-            <CopyToClipboard text={background}>
+            <CopyToClipboard text={background} onCopy={this.changeCopyState}>
                 <div style={{background: background}}className="ColorBox">
+                <div style={{background: background}}className={this.state.copied ? "copy-overlay show" : "copy-overlay"}></div>
+                 <div className={this.state.copied ? "copy-msg show" : "copy-msg"}>
+                     <h1 className="">Copied!</h1>
+                     <p>{this.props.background}</p>
+                 </div>
                     <div className="copy-container">
                         <div className="box-content">
                             <span>{name}</span>
