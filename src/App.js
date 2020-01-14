88169e677ef9659d2de5238eeff8e8ced48c69f5
domcattell <55451364+domcattell@users.noreply.github.com>
2020-01-14 10:08:14 +0000

    added react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-export class Pallete extends Component {
+export class App extends Component {
     render() {
         return (
             <div>
@@ -10,4 +10,4 @@ export class Pallete extends Component {
     }
 }
 
-export default Pallete;
+export default App;
