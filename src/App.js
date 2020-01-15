eb9fb2b22cce171a92e7283e94250e36733cbc5a
domcattell <55451364+domcattell@users.noreply.github.com>
2020-01-15 16:57:50 +0000

    added routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
-import Pallete from './components/Pallete'
-import seedColors from './seedColors'
+import {Route, Switch} from 'react-router-dom';
+import Pallete from './components/Pallete';
+import seedColors from './seedColors';
 import {generatePallete} from './colorHelpers';
 
 export class App extends Component {
     render() {
         return (
             <div>
+                <Switch>
+                    <Route exact path="/" render{() => }/>
+                    <Route exact path="/pallete/:id/" render{() => }/>
+                </Switch>
                 <Pallete pallete={generatePallete(seedColors[5])}/>
             </div>
         );
